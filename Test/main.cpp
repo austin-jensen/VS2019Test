5f1e465d65a7f2cab7c5002b57c45bd5ef9c957c
austin-jensen <aujensen@microsoft.com>
2019-01-07 16:42:09 -0500

    change

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,6 +1,12 @@
 #include "stdio.h"
+#include "assert.h"
 
 int main(void) {
-	printf("_MSC_VER is %d, should be >= 1920", _MSC_VER);
+	if (_MSC_VER < 1920) {
+		printf("_MSC_VER is %d, should be >= 1920\n", _MSC_VER);
+		assert(_MSC_VER >= 1920);
+	}
+	else
+		printf("All good!\n");
 	return 0; 
 }
